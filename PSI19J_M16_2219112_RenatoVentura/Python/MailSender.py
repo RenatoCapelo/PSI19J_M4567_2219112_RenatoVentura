982bd6df67c33c5493fce0429d5708335d06730a
Renato Capelo Ventura <renatocapelo2003@gmail.com>
2020-05-07 13:29:29 +0100

    Versao 0.2 - Important Update
    Associação ao código de Python.
    Viagem de exemplo criada num fprintf,
    para testar o funcionamento do script.

---
+++ PSI19J_M16_2219112_RenatoVentura/Python/MailSender.py
@@ -0,0 +1,61 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import os.path
+
+#abre o ficheiro criado com o email do cliente atual
+f=open("email.txt")
+#abre o recibo criado
+fi=open("recibo.txt")
+
+email= 'portugal.aero@gmail.com'
+password= 'nZ%lQv8!gV2*'
+#como no recibo aparece sempre o numero da reserva na primeira linha
+#consigo fazer com que o subject vá buscar a primeira linha do recibo
+subject= "Recibo da "+fi.readline()
+
+#vai buscar o email do cliente atual
+send_to_email=f.readline()
+
+message='Em anexo segue o recibo da sua reserva. Esperemos que tenha uma otima viagem'
+
+#fecha os ficheiros
+f.close
+fi.close
+
+
+#codigo que faz a comunicação com o servidor
+msg = MIMEMultipart()
+msg['From'] = email
+msg['To'] = send_to_email
+msg['Subject'] = subject
+msg = MIMEMultipart()
+msg['From'] = email
+msg['To'] = send_to_email
+msg['Subject'] = subject
+
+msg.attach(MIMEText(message, 'plain'))
+
+# Setup the attachment
+filename = os.path.basename("recibo.txt")
+attachment = open("recibo.txt", "rb")
+part = MIMEBase('application', 'octet-stream')
+part.set_payload(attachment.read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+# Attach the attachment to the MIMEMultipart object
+msg.attach(part)
+
+
+#deverá se substituir o ip do server smtp caso exita mudança de provedor de email. Neste caso,o ip é o do server de smtp do gmail.
+server = smtplib.SMTP('64.233.184.108')
+server.starttls()
+server.login(email, password)
+text = msg.as_string()
+server.sendmail(email, send_to_email, text)
+server.quit()
+
+#EOF
--- PSI19J_M16_2219112_RenatoVentura/main.c
+++ PSI19J_M16_2219112_RenatoVentura/main.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define num_encomenda_1 "Passagem numero "
+#define num_encomenda_3 ".txt"
 
 int main()
 {
-    printf("Hello world!\n");
+    FILE *ficheiro;
+    FILE *ficheiro_temp;
+    FILE *info_mail;
+    char num_encomenda_2[5];
+    char num_encomenda_final[25];
+    char num_encomenda_final_temp[25];  ///Usada para o fprintf
+    char email[255];
+    printf("Insira o seu email: ");
+    gets(email);
+
+    for(int a=0;a<1;a++)
+    {
+        strcpy(num_encomenda_final, ""); ///Reseta a string
+        sprintf(num_encomenda_2,"%i",a+1); ///Altera o numero de encomenda
+        strcat(num_encomenda_final,num_encomenda_1);
+        strcat(num_encomenda_final,num_encomenda_2);
+        strcpy(num_encomenda_final_temp,num_encomenda_final);
+        strcat(num_encomenda_final,num_encomenda_3);
+
+
+    ficheiro=fopen(num_encomenda_final,"w");
+    ficheiro_temp=fopen("recibo.txt","w");
+    info_mail=fopen("email.txt","w");
+    fprintf(info_mail,email);
+    fprintf(ficheiro,"%s \n Recibo do voo:\n Voo de Lisboa a Barcelona: 70 Eur\n Voo de Barcelona a Lisboa: 65 Eur\n Total: 135 Eur\n",num_encomenda_final_temp);
+    fprintf(ficheiro_temp,"%s \n%s\n Recibo do voo:\n Voo de Lisboa a Barcelona: 70 Eur\n Voo de Barcelona a Lisboa: 65 Eur\n Total: 135 Eur\n",num_encomenda_final_temp,email);
+    fclose(info_mail);
+    fclose(ficheiro);
+    fclose(ficheiro_temp);
+    //system("Python\\emailtest.py");   ///Abre o script de python que envia o email
+    system("Python\\dist\\mailsender.exe"); ///Abre a app em python que envia o email
+    printf("recibo enviado para o seu email\n");
+
+    }
     return 0;
 }
